74f2d0a676e5a384c9e8322b33acc94b33afbbd8
Raja <craja26@gmail.com>
2021-10-24 21:08:30 +0200

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -80,8 +80,24 @@ $ ps -elf --forest
 
 
 
-## List the database privileges using psql
+/***** ## List the database privileges using psql  ******/
 SELECT grantee, privilege_type 
 FROM information_schema.role_table_grants 
 WHERE table_name='mytable'
+
+SELECT grantor, grantee, table_schema, table_name, privilege_type
+FROM information_schema.table_privileges
+WHERE grantee = 'myuser'
+
+SELECT grantee AS user, CONCAT(table_schema, '.', table_name) AS table, 
+    CASE 
+        WHEN COUNT(privilege_type) = 7 THEN 'ALL'
+        ELSE ARRAY_TO_STRING(ARRAY_AGG(privilege_type), ', ')
+    END AS grants
+FROM information_schema.role_table_grants
+GROUP BY table_name, table_schema, grantee;
+
+/*********************/
+
+
 
